285f513cbdd039de080c3704fe9cb3b4b77fdb94
xyz <xyzzhybz@163.com>
2020-08-13 02:42:33 +0800

    no finished t1

--- src/app/App.js
+++ src/app/App.js
@@ -1,14 +1,20 @@
 import React, {Component} from 'react';
 import './app.less';
-import {Route, BrowserRouter} from "react-router-dom";
+import {Route, BrowserRouter,NavLink} from "react-router-dom";
 import Home from "./Home";
+import Calculator from './Calculator';
+import Countdown from './Countdown';
 
 class App extends Component {
   render() {
     return (
       <div className="app">
+        
         <BrowserRouter>
+
           <Route exact path='/' component={Home}/>
+          <Route exact path='/calculator' component={Calculator}/>
+          <Route exact path='/countdown' component={Countdown}/>
         </BrowserRouter>
       </div>
     );
---
+++ src/app/Countdown.js
@@ -0,0 +1,13 @@
+import React, { Component ,NavLink} from 'react'
+
+class Countdown extends Component{
+    render(){
+        return(
+            <div className='countdown'>
+
+            </div>
+        )
+    }
+}
+
+export default Countdown;
